ea56d6976476f6acabbbfc15b57ba3680e18fdf3
mwnmoa <mwnmoa@gmail.com>
2021-12-31 19:04:06 +0700

    ADD Local Storage functionality

--- app.js
+++ app.js
@@ -11,15 +11,8 @@ class Contacts {
 //UI class
 class UI {
   static displayContacts() {
-    const storeContacts = [
-      {
-        firstName: "Monika",
-        lastName: "Kala",
-        email: "emili@o2.pl",
-        phone: "234567876",
-      },
-    ];
-    const contacts = storeContacts;
+    
+    const contacts = Store.getContacts();
     contacts.forEach((contact) => UI.addContactToList(contact));
   }
 
@@ -27,10 +20,10 @@ class UI {
     const list = document.getElementById("contact-list");
     const row = document.createElement("tr");
     row.innerHTML = `
-      <td>${contact.firstName}<td>
-      <td>${contact.lastName}<td>
-      <td>${contact.email}<td>
-      <td>${contact.phone}<td>
+      <td>${contact.firstName}</td>
+      <td>${contact.lastName}</td>
+      <td>${contact.email}</td>
+      <td>${contact.phone}</td>
       <td><i class="bi bi-trash btn btn-primary btn-xs delete"></i></a><i class="btn btn-xs btn-info bi bi-pencil edit"></i></td>
       `;
     list.appendChild(row);
@@ -61,10 +54,42 @@ class UI {
   }
 }
 
-//Event listeners
+class Store {
+  static getContacts() {
+    let contacts;
+    if (localStorage.getItem("contacts") === null) {
+      contacts = [];
+    } else {
+      contacts = JSON.parse(localStorage.getItem("contacts"));
+    }
+    return contacts;
+  }
+
+  static addContact(contact) {
+      const contacts = Store.getContacts();
+      contacts.push(contact);
+      localStorage.setItem('contacts', JSON.stringify(contacts))
+      console.log(contacts)
+  }
+
+  static removeContact(phone) {
+      const contacts = Store.getContacts();
+        contacts.forEach((contact, index)=> {
+            if(contact.phone === phone){
+                contacts.splice(index, 1);
+            }
+        })
+
+    //   contacts.filter((contact) => {contact.phone !== phone})
+     localStorage.setItem('contacts', JSON.stringify(contacts))
+  }
+
+}
+
+//Display actual Local Storage
 document.addEventListener("DOMContentLoaded", UI.displayContacts);
 
-//Event Add Contact Button
+//Add Contact Buttn Functionality
 document.querySelector(".contact-form").addEventListener("submit", (event) => {
   event.preventDefault();
   const firstName = document.getElementById("first-name").value;
@@ -72,20 +97,23 @@ document.querySelector(".contact-form").addEventListener("submit", (event) => {
   const email = document.getElementById("email").value;
   const phone = document.getElementById("phone-number").value;
 
+  //Form validation
   if (firstName === "" || lastName === "" || email === "" || phone === "") {
     UI.showAlert("Please fill all fields", "primary");
   } else {
+      
     const contact = new Contacts(firstName, lastName, email, phone);
-
     UI.addContactToList(contact);
+    Store.addContact(contact)
     UI.clearFields();
     UI.showAlert("Contact Added!", "success");
   }
 });
 
-//event delete
+//Delete Button Functionality
 document.getElementById("contact-list").addEventListener("click", (event) => {
-  // event.preventDefault();
   UI.deleteContact(event.target);
   UI.showAlert("Contact Deleted!", "danger");
+  //getting the phone from the store and removing whole object from storage
+  Store.removeContact(event.target.parentElement.previousElementSibling.innerText)
 });
